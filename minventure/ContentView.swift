d35144be02352669f165513397f0105e643c9739
Andy Sherwood <xinmyname@gmail.com>
2019-12-15 11:46:54 -0800

    SwiftUI attempt

---
+++ minventure/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  minventure
+//
+//  Created by Andy Sherwood on 12/15/19.
+//  Copyright © 2019 Andy Sherwood. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
