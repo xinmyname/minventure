b23dac9005c133c219e249a14b5d59cdbfed36ff
Andy Sherwood <xinmyname@gmail.com>
2019-12-15 13:00:37 -0800

    Initial layout

--- minventure/ContentView.swift
+++ minventure/ContentView.swift
@@ -10,7 +10,76 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+    
+        VStack(alignment: .leading) {
+            
+            Text("minventure").font(.largeTitle)
+            
+            Divider()
+            
+            HStack() {
+
+                VStack(alignment: .leading) {
+                    Text("health")
+                    Text("level")
+                    Text("experience")
+                    Text("gold")
+                }
+                .padding(.trailing)
+
+                VStack(alignment: .leading) {
+                    Text("32/32")
+                    Text("1")
+                    Text("0")
+                    Text("0")
+                }
+            }
+            .padding(.top)
+
+            VStack() {
+                VStack() {
+                    HStack() {
+                        Text("forest!")
+                        Spacer()
+                        Text("3")
+                    }
+                }
+                .padding()
+                .background(Color.green)
+            }
+            .padding(.vertical)
+            
+            VStack(alignment: .leading, spacing: 16) {
+            
+                Button(action: {}) {
+                    Text("move!")
+                        .foregroundColor(Color.pink)
+                        .padding(8)
+                }
+                .frame(maxWidth: .infinity)
+                .border(Color.pink, width: 4)
+
+                Button(action: {}) {
+                    Text("fight!")
+                        .foregroundColor(Color.pink)
+                        .padding(8)
+                }
+                .frame(maxWidth: .infinity)
+                .border(Color.pink, width: 4)
+
+                Button(action: {}) {
+                    Text("rest!")
+                        .foregroundColor(Color.pink)
+                        .padding(8)
+                }
+                .frame(maxWidth: .infinity)
+                .border(Color.pink, width: 4)
+            }
+            .frame(maxWidth: .infinity)
+            
+        }
+        .frame(maxHeight: .infinity, alignment: .top)
+        .padding()
     }
 }
 
